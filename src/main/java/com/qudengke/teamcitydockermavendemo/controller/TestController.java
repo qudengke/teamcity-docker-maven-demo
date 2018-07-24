d321825c7191c5cc6ef3022a93144fe5c489c539
qvdengke <qudengke@sunlands.com>
2018-07-24 17:07:32 +0800

    测试

--- src/main/java/com/qudengke/teamcitydockermavendemo/controller/TestController.java
+++ src/main/java/com/qudengke/teamcitydockermavendemo/controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "test";
+        return "master";
     }
 }
