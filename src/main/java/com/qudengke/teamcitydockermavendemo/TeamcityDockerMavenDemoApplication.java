c6f10aac9b782c22e2be0f9b6c96ee4c6da232bc
qvdengke <qudengke@sunlands.com>
2018-07-24 10:45:31 +0800

    first commit

---
+++ src/main/java/com/qudengke/teamcitydockermavendemo/TeamcityDockerMavenDemoApplication.java
@@ -0,0 +1,12 @@
+package com.qudengke.teamcitydockermavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityDockerMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcityDockerMavenDemoApplication.class, args);
+	}
+}
